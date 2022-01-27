0c52fb26e0ced47588c504b4d72652a62894fa3f
Hikmahx <hikmayousuph@gmail.com>
2022-01-27 22:33:23 +0100

    colors and font families configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors : {
+      'bright-orange': '#e38826', 
+      'dark-cyan': '#006970',
+      'very-dark-cyan': '#004241',
+      'light-gray': '#f2f2f2',
+      'transparent-white':'#ffffffbf'
+    },
+    fontFamily : {
+      'lexend-deca': ['"Lexend Deca"', 'sans-serif'], 
+      'big-shoulders': ['"Big Shoulders Display"', 'cursive']
+    }
   },
   plugins: [],
 }
