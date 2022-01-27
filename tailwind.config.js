c3c667069d8750703eca80063b331198921d0e4f
Hikmahx <hikmayousuph@gmail.com>
2022-01-28 00:28:07 +0100

    new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
       'dark-cyan': '#006970',
       'very-dark-cyan': '#004241',
       'light-gray': '#f2f2f2',
-      'transparent-white':'#ffffffbf'
+      'transparent-white':'#ffffffbf', 
+      'transparent': 'transparent',
+      'white': '#ffffff'
     },
     fontFamily : {
       'lexend-deca': ['"Lexend Deca"', 'sans-serif'], 
